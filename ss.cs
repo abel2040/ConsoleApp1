d3685f200898a06d838791c14ea9ed6ba1006f9c
abel2040 <Kirosa142@gmail.com>
2022-06-24 13:15:01 +0300

    reacving the frist to numbers

--- ss.cs
+++ ss.cs
@@ -11,9 +11,15 @@ public class Class1
             {
 				try
                 {
-					console.writeline("  enter '+' for adition  ");
-					console.writeline("  enter '+'  ");
+					console.writeline("  enter '+' for adition ");
+					console.writeline("  enter '-' for subtraction ");
+					console.writeline("  enter '*' for multiplication ");
+					console.writeline("  enter '/' for Division ");
 
+					char read = Convert.Tochar(conlose.ReadLine());
+					if (read == '+')
+						concole.writeline("enter frist number ");
+					double num1 = Convert.ToDouble(concole.ReadLine());
 
 				}
 
