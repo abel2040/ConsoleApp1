8cb1eac9a1448e61f26ed0fa9ab8c1a7910591de
abel2040 <Kirosa142@gmail.com>
2022-07-05 14:18:44 +0300

    create abstract class named ishape and calculater area and parameter

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class ishape
+    {
+        abstract public double calculatearea();
+        abstract public double calculateparameter();
+    }
+    class circle : ishape
+    {
+        public static readonly double pi = 3.14;
+        public int radius { set; get; }
+
+        public override double calculatearea()
+        {
+            double area = radius * radius * pi;
+            return area;
+        }
+
+        public override double calculateparameter()
+        {
+            double parameter = 2 * pi * radius;
+            return parameter;
+        }
+
+
+    }
